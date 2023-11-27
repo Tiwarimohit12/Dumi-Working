f6435e0007d5e540cd5aaaf7c023132e2914a7c9
Mohit Tiwari <145648854+Tiwarimohit12@users.noreply.github.com>
2023-11-27 23:24:11 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,45 @@
+gsap.from("#main1 h2",{
+    y:-100,
+    // rotate:720,
+    duratio:0.7,
+    delay:0.3,
+})
+gsap.from("#main1 h3",{
+    x:-100,
+    // rotate:720,
+    duratio:0.7,
+    delay:0.7,
+})
+gsap.from("#main1 img",{
+    y:-100,
+    rotate:720,
+    duratio:0.7,
+    delay:0.5,
+})
+gsap.from("#main2 h1",{
+    x:-100,
+    duratio:0.7,
+    delay:0.3,
+})
+gsap.t0("#q h1",{
+    x:-50,
+    duratio:1,
+    delay:0.6,
+})
+gsap.from("#y h5",{
+    y:-100,
+    duratio:0.7,
+    delay:0.3,
+    rotate:720,
+})
+gsap.from("#main3 h1",{
+    y:-100,
+    duratio:0.7,
+    delay:0.3,
+    rotate:720,
+})
+gsap.from("#main3 #r",{
+    delay:1,
+    opacity:0,
+    scale:0.5
+})
